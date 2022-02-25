3cad3977ae3eef5ec783e343b6f811b6fd5f4490
heisenberg2048 <erjaymandal@gmail.com>
2022-02-26 02:03:50 +0530

    js file added
    still not completed js file

---
+++ app.js
@@ -0,0 +1,37 @@
+const newtask = document.querySelector("#newtask");
+const addbutton = document.querySelector("#addbutton");
+const toappend = document.querySelector("#toappend");
+
+
+addbutton.addEventListener('click', function (event) {
+
+    event.preventDefault();
+    console.log(newtask.value);
+
+    const newrow = document.createElement('div');
+    newrow.classList.add("row", "container");
+
+    const taskcheckbox = document.createElement('input');
+    taskcheckbox.type = 'checkbox';
+    taskcheckbox.name = 'taskcheckbox';
+    taskcheckbox.classList.add('taskcheckbox');
+
+
+    const task = document.createElement('label');
+    task.for = 'taskcheckbox';
+    task.innerText = newtask.value;
+    task.classList.add('task');
+
+    const checktask = document.createElement('div');
+    checktask.append(taskcheckbox);
+    checktask.append(task);
+
+    newrow.append(checktask);
+
+
+    toappend.append(newrow);
+
+    newtask.value = "";
+
+
+});
